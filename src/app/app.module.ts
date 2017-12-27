8d5dd8449d8634f805b85072ed75b5b15c93ac00
xiongMingCai <xmc000@icloud.com>
2017-12-27 22:21:10 +0800

    组件拆分

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import { PageHeaderComponent } from './page-header/page-header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
